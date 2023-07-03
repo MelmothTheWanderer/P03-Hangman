d34ca72cd1f716b6505521eb248e4610ae703c83
MelmothTheWanderer <>
2023-07-03 11:59:05 +0000

    Inital commit

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+#This is a comment to test that things are working on Github , and that  Codeanywhere is pushing to Github okay . 
+
+def greeting (name):
+    print(f"Hello , my name is {name}")
+
+greeting(input("Please type your name here: \n"))
+

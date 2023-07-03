9ec5ccf382676fdcc2c9d8f2ae79a9ba17766ac3
MelmothTheWanderer <>
2023-07-03 13:51:41 +0000

    added a list of possible words in an external file caled words.py

--- run.py
+++ run.py
@@ -2,7 +2,9 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+import random 
+from words import list_of_words
 
-#This is a comment to test that things are working on Github , and that  Codeanywhere is pushing to Github okay . 
 
-
+for _ in range(10):
+    print(random.choice(list_of_words))
---
+++ words.py
@@ -0,0 +1,8 @@
+list_of_words = ['needle', 'exuberant', 'approve', 'boil', 'careless', 'harsh', 'person', 'unpack', 'large', 'spring', 'mind', 'decay', 'provide',\
+'smile', 'late', 'bath', 'throat', 'long', 'dime', 'rescue', 'stormy', 'concerned', 'cakes', 'funny', 'satisfy', 'hug', 'tax', 'bite-sized',\
+'fowl', 'water', 'lackadaisical', 'joke', 'ants', 'organic', 'wealthy', 'wish', 'wise', 'general', 'ball', 'tease', 'tangy', 'limping',\
+'airplane', 'vengeful', 'discussion', 'perpetual', 'outrageous', 'yell', 'show', 'stick', 'rejoice', 'yarn', 'potato', 'unfasten',\
+'type', 'trot', 'suggestion', 'irritate', 'freezing', 'insidious', 'multiply', 'tin', 'wrestle', 'moan', 'engine', 'abortive', \
+'save', 'avoid', 'flowers', 'complete', 'ski', 'grandiose', 'crash', 'relieved', 'cooing', 'protest', 'dysfunctional', 'good',\
+'yielding', 'fierce', 'crabby', 'worthless', 'glib', 'utter', 'amusing', 'produce', 'billowy', 'snotty', 'paddle',\
+'scandalous', 'reading', 'loss', 'joyous', 'flash', 'guarantee', 'pies', 'faithful', 'invent', 'actor']

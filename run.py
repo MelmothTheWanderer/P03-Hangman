c12864b8ce273682cae67ff8f621a6148403c22f
Jamie Simms <jamiesimms@protonmail.com>
2023-07-05 19:07:59 +0100

    added a function that will loop the game until the player either wins or loses

--- run.py
+++ run.py
@@ -93,29 +93,30 @@ class Game:
 
             self.refresh_the_playboard()
             print("")
-            print("That's a hit!")
+            print("That's a hit!\nKeep it up!")
 
         else:
             self.refresh_the_playboard()
+            self.lives -= 1
+            self.refresh_the_playboard()
             print("")
-            print("Nope, that letter is not in the word!")
-
-    # TODO: Define a function that will combine the various functions involved in "refreshing"
-    #    every time the user makes a guess.
-
-
+            print("Nope, that letter is not in the word\nGuess again!")
 
     # TODO Make a function that will keep allowing the user to make these guesses until they
     #   there are no underscores present in the display array , thus winnind the game, or
     #   until they run out of lives, thus losing the game.
 
+    # As long as there are still letters to guess, and the player has at least 1 life:
+    def play_game(self):
+        while "_" in self.display and self.lives > 0:
+            # 1. Display the game data
+            self.refresh_the_playboard()
+            # 2. Ask user to make a guess
+            self.check_guess(self.make_guess())
+    # 3 Update the data (this is done in the check_guess function
+
 
 New_game = Game()
 
 New_game.print_out_logo()
-print(New_game.word)
-
-New_game.print_out_logo_art()
-New_game.print_word_display()
-New_game.check_guess(New_game.make_guess())
-
+New_game.play_game()
